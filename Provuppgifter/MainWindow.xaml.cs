c97d9862be48c7eee0a463c10c4826c996c0f465
Uzayon Imtiaz <uzayon.imtiaz@elev.ga.ntig.se>
2025-03-24 14:44:14 +0100

    Tränat på XAML och C# - events.

---
+++ Provuppgifter/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Provuppgifter;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void BeräknaPorto(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Du klickade på Beräkna porto");
+        
+        // string text = BeräknaPorto.tbxVikt;
+        // MessageBox.Show($"Du track in: {BeräknaPorto}");
+    }
+}
